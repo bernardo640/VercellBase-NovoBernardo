74910d5a752bf1539f08ed74f4469cce1ff8b7a9
bernardo640 <bernardooliveira.bg006@academico.ifsul.edu.br>
2025-09-25 10:53:55 -0300

    “se-leu-e-gay”

--- controllers/YoutubeController.js
+++ controllers/YoutubeController.js
@@ -1,50 +1,61 @@
+import Canal from '../models/canal.js';
 import Youtube from '../models/youtube.js';
 
 export default class YoutubeController {
-  constructor() {
-    // Caminho base das views
-    this.caminhoBase = 'youtube/';
-  }
+  constructor(caminhoBase = 'youtube/') {
+    this.caminhoBase = caminhoBase;
 
-  // Renderiza formulário de cadastro
-  openAdd = (req, res) => {
-    res.render(this.caminhoBase + 'add');
-  }
+    // Renderiza formulário de cadastro
+    this.openAdd = async (req, res) => {
+      const canais = await Canal.find({});
+      res.render(this.caminhoBase + "add", { canais });
+    };
 
-  // Cria novo vídeo
-  add = async (req, res) => {
-    await Youtube.create(req.body);
-    res.redirect('/youtube/add');
-  }
+    // Cria novo vídeo
+    this.add = async (req, res) => {
+      await Youtube.create({
+        titulo: req.body.titulo,
+        canal: req.body.canal, // ObjectId de Canal
+        duracao: req.body.duracao,
+        visualizacoes: req.body.visualizacoes,
+        upload: req.body.upload,
+        acoes: req.body.acoes
+      });
+      res.redirect('/' + this.caminhoBase + 'add');
+    };
 
-  // Lista vídeos
-  list = async (req, res) => {
-    const resultado = await Youtube.find({});
-    res.render(this.caminhoBase + 'lst', { Youtube: resultado });
-  }
+    // Lista vídeos (com dados do Canal)
+    this.list = async (req, res) => {
+      const resultado = await Youtube.find({}).populate("canal");
+      res.render(this.caminhoBase + 'lst', { videos: resultado });
+    };
 
-  // Pesquisa vídeos
-  find = async (req, res) => {
-    const filtro = req.body.filtro || '';
-    const resultado = await Youtube.find({ titulo: { $regex: filtro, $options: 'i' } });
-    res.render(this.caminhoBase + 'lst', { Youtube: resultado });
-  }
+    // Pesquisa vídeos por título (com dados do Canal)
+    this.find = async (req, res) => {
+      const filtro = req.body.filtro || '';
+      const resultado = await Youtube.find({
+        titulo: { $regex: filtro, $options: "i" }
+      }).populate("canal");
+      res.render(this.caminhoBase + 'lst', { videos: resultado });
+    };
 
-  // Renderiza formulário de edição
-  openEdt = async (req, res) => {
-    const youtube = await Youtube.findById(req.params.id);
-    res.render(this.caminhoBase + 'edt', { Youtube: youtube });
-  }
+    // Renderiza formulário de edição
+    this.openEdt = async (req, res) => {
+      const canais = await Canal.find({});
+      const youtube = await Youtube.findById(req.params.id).populate("canal");
+      res.render(this.caminhoBase + "edt", { youtube, canais });
+    };
 
-  // Atualiza vídeo
-  edt = async (req, res) => {
-    await Youtube.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect('/youtube/lst');
-  }
+    // Atualiza vídeo
+    this.edt = async (req, res) => {
+      await Youtube.findByIdAndUpdate(req.params.id, req.body);
+      res.redirect('/' + this.caminhoBase + 'lst');
+    };
 
-  // Deleta vídeo
-  del = async (req, res) => {
-    await Youtube.findByIdAndDelete(req.params.id);
-    res.redirect('/youtube/lst');
+    // Deleta vídeo
+    this.del = async (req, res) => {
+      await Youtube.findByIdAndDelete(req.params.id);
+      res.redirect('/' + this.caminhoBase + 'lst');
+    };
   }
 }
--- models/youtube.js
+++ models/youtube.js
@@ -1,12 +1,16 @@
-import conexao from '../config/conexao.js'
+import conexao from '../config/conexao.js';
 
 const YoutubeSchema = conexao.Schema({
     titulo: { type: String, required: true },
-    canal: { type: String, required: true },
+    canal: { 
+        type: conexao.Types.ObjectId, 
+        ref: 'Canal', // 🔗 relação com Canal
+        required: true 
+    },
     duracao: { type: Number, required: true },
     visualizacoes: { type: Number, required: true },
-    upload: { type: String, required: true },
+    upload: { type: Date, required: true },
     acoes: { type: String, required: true }
-})
+});
 
-export default conexao.model('Youtube', YoutubeSchema)
+export default conexao.model('Youtube', YoutubeSchema);
